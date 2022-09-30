d44a3b91be96a97ec83dc58ad91a27d02553206c
Maha <kaverron1@gmail.com>
2022-09-30 13:44:03 +0300

    dop4js

---
+++ main.js
@@ -0,0 +1,45 @@
+// ////////////////////////////////////////////////////////////////////////// TASK 1
+// let b = document.querySelector('ul')
+// b.remove()
+
+// const a = document.body;
+// a.innerHTML = `<ul><li class="list-item green">green</li></ul>`
+////////////////////////////////////////////////////////////////////////////// TASK 8
+// const input1 = document.querySelector('#secret-login');
+// input1.value = 'You were hacked'
+/////////////////////////////////////////////////////////////////////////////// TASK 2
+// let a = document.querySelectorAll('li');
+// let ul = document.querySelector('ul')
+// let o = Array.from( Object.values(a))
+// let d =  o.forEach((item)=>{
+//   return(  ul.inerHTML=`
+    
+//     <li>${item}</li>
+//     `)
+// })
+// console.log(d)
+//////////////////////////////////////////////////////////////////////////////// TASK 3
+
+    // const listitem = document.querySelectorAll('li.list-item')
+
+    // function arrayList () {
+    //     return Array(listitem)
+    // }
+    // console.log(listitem);
+
+
+/////////////////////////////////////////////////////////////////////////////// TASK 4
+// const dataTest = document.querySelectorAll('li')
+
+// const firstData = dataTest[0]
+// console.log(firstData);
+// const secondData = dataTest[1]
+// console.log(secondData);
+// const thirdData = dataTest[2]
+// console.log(thirdData);
+// const fourthData = dataTest[3]
+// console.log(fourthData);
+// const fifthData = dataTest[4]
+// console.log(fifthData);
+
+/////////////////////////////////////////////////////////////////////////////// TASK 5
